4c62e5f6758436b3742890719a556d8e6a81b8c1
MinJung0131 <77926129+MinJung0131@users.noreply.github.com>
2024-04-21 23:46:38 +0900

    Title: 데이터 교환, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240421/데이터 교환/exchange-data.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5, b=6, c=7, temp=0;
+
+        temp=a;
+        a=c;
+        c=b;
+        b=temp;
+        
+        System.out.print(a+"\n"+b+"\n"+c);
+
+    }
+}
